930a1be00dbf5638f10739e4ea7f487cb665f73f
Kwasniok <7516208+Kwasniok@users.noreply.github.com>
2025-08-18 11:02:35 +0200

    Fix version in API doc

--- astra_web/main.py
+++ astra_web/main.py
@@ -84,6 +84,7 @@ app = FastAPI(
     description="This is an API wrapper for the ASTRA simulation code developed \
                  by K. Floettmann at DESY Hamburg. For more information, refer to the official \
                  [website](https://www.desy.de/~mpyflo/).",
+    version="2.0.0",
     contact={
         "name": "Jens Kwasniok",
         "email": "jens.kwasniok@desy.de",
