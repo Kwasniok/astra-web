93f4b0602d30256c7adef3285e6d6dd987092b36
Kwasniok <Kwasniok@users.noreply.github.com>
2025-11-07 14:33:41 +0100

    Refactored uuid

--- astra_web/uuid.py
+++ astra_web/uuid.py
@@ -1,7 +1,7 @@
-from shortuuid import uuid
+from shortuuid import uuid as _uuid
 from datetime import datetime
 
 
 def get_uuid() -> str:
     """Generate a new UUID based on the current date and time."""
-    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}-{uuid()[:8]}"
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}-{_uuid()[:8]}"
