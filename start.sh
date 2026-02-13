cb3882e5977b67b425088d2c9b6510bf5f62343e
Kwasniok <Kwasniok@users.noreply.github.com>
2026-02-13 01:36:04 +0100

    Changed requirements location

--- start.sh
+++ start.sh
@@ -58,7 +58,7 @@ if [ ! -d ".venv" ]; then
     python3 -m venv .venv
 fi
 source .venv/bin/activate
-pip install -r requirements/requirements.txt
+pip install -r requirements.txt
 
 
 # ensure ASTRA binaries are installed
